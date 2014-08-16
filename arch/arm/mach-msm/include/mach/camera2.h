68c431bf95976f51dd3c1ae9a77d7c298a962d59
raymanfx <raymanfx@gmail.com>
2014-08-16 21:09:58 +0200

    msm: camera2: add missing vreg entries

--- arch/arm/mach-msm/include/mach/camera2.h
+++ arch/arm/mach-msm/include/mach/camera2.h
@@ -82,6 +82,8 @@ struct msm_camera_sensor_board_info {
 	const char *actuator_name;
 	struct msm_camera_slave_info *slave_info;
 	struct msm_camera_csi_lane_params *csi_lane_params;
+	struct camera_vreg_t *cam_vreg;
+	int num_vreg;
 	struct msm_camera_sensor_strobe_flash_data *strobe_flash_data;
 	struct msm_actuator_info *actuator_info;
 	struct msm_sensor_info_t *sensor_info;

1fac14e7f6c6add3ecfcbc5a66d1c4ea6981a808
raymanfx <raymanfx@gmail.com>
2014-08-17 10:38:51 +0200

    rtc: alarm: add back missing alarm_shutdown code

--- drivers/rtc/alarm.c
+++ drivers/rtc/alarm.c
@@ -180,6 +180,7 @@ static void rtc_task(struct work_struct *work)
 /* OPPO 2013-11-19 yuyi Add end for power up alarm */
 static int set_alarm_time_to_rtc(const long);
 
+static void alarm_shutdown(struct platform_device *dev);
 void set_power_on_alarm(long secs, bool enable)
 {
 	mutex_lock(&power_on_alarm_mutex);
@@ -194,11 +195,11 @@ void set_power_on_alarm(long secs, bool enable)
 		else
 			power_on_alarm = 0;
 	}
+    alarm_shutdown(NULL);
 
 	set_alarm_time_to_rtc(power_on_alarm);
 	mutex_unlock(&power_on_alarm_mutex);
-}
-static void alarm_shutdown(struct platform_device *dev);
+};
 
 static void update_timer_locked(struct alarm_queue *base, bool head_removed)
 {
@@ -866,6 +867,61 @@ static int alarm_resume(struct platform_device *pdev)
 	return 0;
 }
 
+static void alarm_shutdown(struct platform_device *dev)
+{
+	struct timespec wall_time;
+	struct rtc_time rtc_time;
+	struct rtc_wkalrm alarm;
+	unsigned long flags;
+	long rtc_secs, alarm_delta, alarm_time;
+	int rc;
+
+	spin_lock_irqsave(&alarm_slock, flags);
+
+	if (!power_on_alarm) {
+        spin_unlock_irqrestore(&alarm_slock, flags);
+        goto disable_alarm;
+    }
+    spin_unlock_irqrestore(&alarm_slock, flags);
+	
+	/*OPPO yuyi add begin just for analysis boot automaticly*/
+	#ifdef VENDOR_EDIT
+	printk("alarm  alarm_shutdown enable alarm_powerup\n");
+	#endif
+	/*OPPO yuyi add end just for analysis boot automaticly*/
+	
+	rtc_read_time(alarm_rtc_dev, &rtc_time);
+	getnstimeofday(&wall_time);
+	rtc_tm_to_time(&rtc_time, &rtc_secs);
+	alarm_delta = wall_time.tv_sec - rtc_secs;
+	alarm_time = power_on_alarm - alarm_delta;
+
+	/*
+	 * Substract ALARM_DELTA from actual alarm time
+	 * to powerup the device before actual alarm
+	 * expiration.
+	 */
+	if ((alarm_time - ALARM_DELTA) > rtc_secs)
+		alarm_time -= ALARM_DELTA;
+
+	if (alarm_time <= rtc_secs)
+		goto disable_alarm;
+
+	rtc_time_to_tm(alarm_time, &alarm.time);
+	alarm.enabled = 1;
+	rc = rtc_set_alarm(alarm_rtc_dev, &alarm);
+	if (rc)
+		pr_alarm(ERROR, "Unable to set power-on alarm\n");
+	else
+		pr_alarm(FLOW, "Power-on alarm set to %lu\n",
+				alarm_time);
+
+	return;
+
+disable_alarm:
+	rtc_alarm_irq_enable(alarm_rtc_dev, 0);
+}
+
 static int set_alarm_time_to_rtc(const long power_on_time)
 {
 	struct timespec wall_time;
@@ -994,6 +1050,7 @@ static struct class_interface rtc_alarm_interface = {
 static struct platform_driver alarm_driver = {
 	.suspend = alarm_suspend,
 	.resume = alarm_resume,
+    .shutdown = alarm_shutdown,
 	.driver = {
 		.name = "alarm"
 	}
